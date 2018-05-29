2df107d8a0f1d1fb27d9056ab62e0cb05a218a15
Stacie Taylor-Cima <stacietaylorcima@Stacies-MacBook-Air.local>
2018-05-29 10:18:24 -0700

    create API EventsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    match '/events', to: 'events#preflight', via: [:options]
+    resources :events, only: [:create]
+  end
 
   devise_for :users
+
+  resources :registered_applications
+
   get 'welcome/index'
   get 'welcome/about'
-  resources :registered_applications 
 
   root 'welcome#index'
 end
